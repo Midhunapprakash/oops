a0f73d18b70fce53d98dba4ba8b7ae209d35eb7b
Midhunapprakash <midhunapprakash66@gmail.com>
2025-05-02 11:45:45 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,46 +1,32 @@
-class Product{
-  String pcode;
-  String pname;
-  double price;
- public void setProductDetails(String productCode,String productName,double productPrice){
-  pcode=productCode;
-  pname=productName;
-  price=productPrice;
-}
-public double getPrice(){
-  return price;
-}
-public void displayProduct(){
-    System.out.println("productCode:"+pcode);
-    System.out.println("productName:"+pname);
-    System.out.println("productPrice:"+price);
-}
-}
-
-public class Main{
-  public static void main(String[] args){
-        Product product1=new Product();
-        product1.setProductDetails("P001","Product1",100.0);
-
-        Product product2=new Product();
-        product2.setProductDetails("P002","Product2",50.0);
-
-        Product product3=new Product();
-        product3.setProductDetails("P003","Product3",150.0);
-
-        Product lowestPriceProduct = product1;
-
-        if(product2.getPrice()< lowestPriceProduct.getPrice()){
-           lowestPriceProduct = product2;
-           }
-        if(product3.getPrice()< lowestPriceProduct.getPrice()){
-           lowestPriceProduct = product3;
-           }
-
-       System.out.println("\nProduct with the lowest price:");
-       lowestPriceProduct.displayProduct();
-     }
-}
-
-
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         
+        // Read the value of n for Fibonacci series
+        System.out.print("Enter the value of n for Fibonacci series: ");
+        int n = scanner.nextInt();
+
+        // Read the range [start, end]
+        System.out.print("Enter the range (start end): ");
+        int start = scanner.nextInt();
+        int end = scanner.nextInt();
+
+        // Create instances of both classes
+       
+        EvenNumberPrinter evenNumberPrinter = new EvenNumberPrinter(start, end);
+ // Create threads for both tasks
+	Thread evenThread = new Thread(evenNumberPrinter);
+ // Start both threads
+	evenThread.start();
+ 
+ FibonacciGenerator fibonacciGenerator = new FibonacciGenerator(n);
+       
+        Thread fibonacciThread = new Thread(fibonacciGenerator);
+        
+
+       
+        fibonacciThread.start();
+        
+    }
+}
